c622df13496f9b728f7de8c1a56fa8e12eec5ccd
Emilse Arias <emilsearias@gmail.com>
2023-06-06 10:53:42 -0300

    agregamos las rutas en los botones del navbar

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,18 +1,18 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
-
+import { Link, NavLink} from 'react-router-dom';
 
 const Menu = () => {
   return (
     <Navbar bg="danger" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Cafecito</Navbar.Brand>
+        <Navbar.Brand as={Link} to='/'>Cafecito</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#">Inicio</Nav.Link>
-            <Nav.Link href="#">Registro</Nav.Link>
-            <Nav.Link href="#">Administrador</Nav.Link>
-            <Nav.Link href="#">Login</Nav.Link>
+            <NavLink end to={'/'} className={'nav-item nav-link'}>Inicio</NavLink>
+            <NavLink end to={'/registro'} className={'nav-item nav-link'}>Registro</NavLink>
+            <NavLink end to={'/administrador'} className={'nav-item nav-link'}>Administrador</NavLink>
+            <NavLink end to={'/login'} className={'nav-item nav-link'}>Login</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
