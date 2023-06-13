ca7b671a42b281038acb76df76e4badd84261071
Emilse Arias <emilsearias@gmail.com>
2023-06-13 10:28:50 -0300

    agregamos las validaciones al formulario de login

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,6 +1,12 @@
 // llamar la variable de entorno
 const URL_usuario = import.meta.env.VITE_API_USUARIO;
 
+/*
+GET devuelven una lista de elementos, puede devolver un elemento.
+POST permiten agregar elementos.
+PUT / PATCH permiten modificar elementos.
+DELETE  permiten eliminar un elementos.
+*/ 
 export const login = ()=>{
     console.log(URL_usuario)
 }
--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,26 +1,40 @@
 
 import { Form, Button, Container, Card } from "react-bootstrap";
 import { login } from "../helpers/queries";
+import { useForm } from "react-hook-form";
 
 
 const Login = () => {
+  const { register, handleSubmit, formState: { errors}, reset } = useForm();
 
 login();
+
+const onSubmit = (usuario)=>{
+  console.log(usuario);
+}
 
   return (
     <Container className="mainSection">
       <Card className="my-5">
         <Card.Header as="h5">Login</Card.Header>
         <Card.Body>
-          <Form >
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
               <Form.Control
-                type="email"
+                type="text"
                 placeholder="Ingrese un email"
-               
+               { ...register('email', {
+                required:'El email es un dato obligatorio',
+                pattern: {
+                  value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                  message:'El email debe tener un formato valido (mail@dominio.com)'
+                }
+               })}
               />
-             
+              <Form.Text className="text-danger">
+               { errors.email?.message}
+              </Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -28,9 +42,19 @@ login();
               <Form.Control
                 type="password"
                 placeholder="Password"
-               
+               {
+                ...register('password',{
+                  required: 'El password es un dato obligatorio',
+                  pattern:{
+                    value: /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/,
+                    message: 'El password debe contener entre 8 y 16 caracteres y debe incluir numeros, caracteres en mayuscula, miniscula y almenos un caracter especial'
+                  }
+                })
+               }
               />
-            
+             <Form.Text className="text-danger">
+               { errors.password?.message}
+              </Form.Text>
             </Form.Group>
             <Button variant="primary" type="submit">
               Ingresar
