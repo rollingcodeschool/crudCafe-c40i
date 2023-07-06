cfc3bb2848e5d0ff1b21e9ab371ca3bd559cf8aa
Emilse Arias <emilsearias@gmail.com>
2023-07-06 10:18:48 -0300

    completamos el codigo faltante con el repo de diego, modificamos los id por _id

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,8 +1,17 @@
 import { Container, Row } from "react-bootstrap";
 import CardProducto from "./producto/CardProducto";
+import { useEffect,useState } from "react";
+import { obtenerProductos } from "../helpers/queries";
 const Inicio = () => {
+  const [productos,setProductos] = useState([])
+  useEffect(()=>{
+    obtenerProductos().then(res=>{
+      setProductos(res)
+    })
+  },[])
+
   return (
-    <section className="mainSection">
+  <section className="mainSection">
       <img
         className="banner"
         src="https://images.pexels.com/photos/6802983/pexels-photo-6802983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -12,10 +21,7 @@ const Inicio = () => {
         <h1 className="display-4">Nuestros Productos</h1>
         <hr />
         <Row>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
+            {productos.map(producto => <CardProducto key={producto._id} producto={producto}></CardProducto>)}
         </Row>
       </Container>
     </section>
